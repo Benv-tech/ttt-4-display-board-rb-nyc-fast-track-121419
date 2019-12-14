38599da44f7311434caf811fab04a9a4ecfee04f
Benjamin J Velazquez <benvelazquez3@gmail.com>
2019-12-14 23:28:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board=[ " ", " ", " ", " ", "  ", " ", " ", " ", " "])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  puts "-----------"
-puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 display_board
